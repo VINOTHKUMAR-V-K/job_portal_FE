7195b555481a3a14157df722beb46cb9668bb78d
vinothkumarfreelancer <vinothkumar.vkfreelancer@gmail.com>
2025-03-14 17:01:09 +0530

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,4 @@
+export const USER_API_ENDPOINT = "https://job-portal-be-de0f.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://job-portal-be-de0f.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://job-portal-be-de0f.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://job-portal-be-de0f.onrender.com/api/company";
